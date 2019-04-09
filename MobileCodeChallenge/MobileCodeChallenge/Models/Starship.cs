b5e3ff378359b34f0e3f633bb29a05f9e250f177
Nate Robison <ntr2012@yahoo.com>
2019-04-08 22:35:37 -0700

    Add Starship and StarshipList models to project

---
+++ MobileCodeChallenge/MobileCodeChallenge/Models/Starship.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MobileCodeChallenge.Model
+{
+   public class Starship
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("model")]
+        public string Model { get; set; }
+        [JsonProperty("starship_class")]
+        public string StarshipClass { get; set; }
+        [JsonProperty("manufacturer")]
+        public string Manufacturer { get; set; }
+        [JsonProperty("cost_in_credits")]
+        public string CostInCredits { get; set; }
+        [JsonProperty("length")] 
+        public string Length { get; set; }
+        [JsonProperty("crew")]
+        public string Crew { get; set; }
+        [JsonProperty("passengers")]
+        public string Passengers { get; set; }
+        [JsonProperty("max_atmosphering_speed")]
+        public string MaxAtmospheringSpeed { get; set; }
+        [JsonProperty("hyperdrive_rating")]
+        public string HyperdriveRating { get; set; }
+        [JsonProperty("MGLT")]
+        public string MGLT { get; set; }
+        [JsonProperty("cargo_capacity")]
+        public string CargoCapacity { get; set; }
+        [JsonProperty("consumables")]
+        public string Consumables { get; set; }
+
+    }
+}
---
+++ MobileCodeChallenge/MobileCodeChallenge/Models/StarshipList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MobileCodeChallenge.Model
+{
+    public class StarshipList
+    {
+        [JsonProperty("count")]
+        public int Count { get; set; }
+        [JsonProperty("next")]
+        public string Next { get; set; }
+        [JsonProperty("previous")]
+        public string Previous { get; set; }
+        [JsonProperty("results")]
+        public List<Starship> Results { get; set; }
+    }
+}
